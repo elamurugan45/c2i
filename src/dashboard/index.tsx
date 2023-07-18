2b772ec68c12eb309f75810a0588c27318f89927
elamurugan45 <elamuruganraju4545@gmail.com>
2023-07-18 10:43:35 +0530

    design

--- src/dashboard/index.tsx
+++ src/dashboard/index.tsx
@@ -1,20 +1,51 @@
-import { Card, Col, Row } from "antd"
+import { Card, Col, Row } from "antd";
 import "./style.css";
 
-export const Dashboard=()=>{
-    return(
-        <>
+export const Dashboard = () => {
+  return (
+    <>
       <Row gutter={24} className="row-content">
-      Welcome to Connect2Investors!
-        </Row>
-        <Row>
-        <Col>
-         <Card className="cardbox">
+        Welcome to Connect2Investors!
+      </Row>
+      <Row>
+        <Col span={8}>
+          <Card className="cardbox">
             <Row className="text-font">No.Of.Investor</Row>
             <Row className="num-size">20</Row>
-         </Card>
+            <Row>
+              <Col className="text-color">Year of 2023</Col>
+            </Row>
+            <Col>
+              <img src="../assests/savings.png" className="image-size"></img>
+            </Col>
+          </Card>
         </Col>
-        </Row>
-        </>
-        )
-}
+        <Col span={8}>
+          <Card className="cardbox">
+            <Row className="text-font">No.Of.Investor</Row>
+            <Row className="num-size">20</Row>
+            <Row>
+              <Col className="text-color">Year of 2023</Col>
+            </Row>
+            <Col>
+              <img src="../assests/partner_exchange.png" className="image-size"></img>
+            </Col>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card className="cardbox">
+            <Row className="text-font">No.Of.Investor</Row>
+            <Row className="num-size">20</Row>
+            <Row>
+              <Col className="text-color">Year of 2023</Col>
+            </Row>
+            <Col>
+              <img src="../assests/rocket_launch.png" className="image-size"></img>
+            </Col>
+          </Card>
+        </Col>
+      </Row>
+
+    </>
+  );
+};
